ed38831b729a677b2346a7a855785a4f37e76c53
daskrohn <lynne.krohn@mail.mcgill.ca>
2020-04-16 13:16:45 -0400

    updated parse files R to make it hard calls

--- STEP5_prep-input-files.R
+++ STEP5_prep-input-files.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
-#PARSE INTO FILES
+# PARSE INTO FILES
 # run in the same folder as imputation results. 
+# this is for hard calls (Rsq>=0.08 for imputed variants). For soft calls, change to Rsq >= 0.30
 
 R
 
@@ -11,23 +12,23 @@ for(i in 1:22)
 {
   input <- paste("chr",i,".info.gz", sep = "")
   data <- read.table(input, header = T)
-  dat <- subset(data, MAF >= 0.001 & Rsq >= 0.30)
+  dat <- subset(data, MAF >= 0.001 & Rsq >= 0.80)
   dat$chr <- ldply(strsplit(as.character(dat$SNP), split = ":"))[[1]]
   dat$bp <- ldply(strsplit(as.character(dat$SNP), split = ":"))[[2]]
   da <- dat[,c("SNP","ALT_Frq","Rsq")]
-  write.table(da, paste("maf001rsq03minimums_chr",i,".info",sep = ""), row.names = F, quote = F, sep = "\t")
+  write.table(da, paste("maf001rsq08minimums_chr",i,".info",sep = ""), row.names = F, quote = F, sep = "\t")
 }
 
 for(i in 1:22)
 {
   input <- paste("chr",i,".info.gz", sep = "")
   data <- read.table(input, header = T)
-  dat <- subset(data, MAF >= 0.001 & Rsq >= 0.30)
+  dat <- subset(data, MAF >= 0.001 & Rsq >= 0.80)
   dat$chr <- ldply(strsplit(as.character(dat$SNP), split = ":"))[[1]]
   dat$bp <- ldply(strsplit(as.character(dat$SNP), split = ":"))[[2]]
   dat$range <- paste(dat$chr, ":", dat$bp, "-", dat$bp, sep = "")
   da <- dat[,c("range")]
-  write.table(da, paste("maf001rsq03minimums_chr",i,".txt",sep = ""), col.names = F, row.names = F, quote = F, sep = "\t")
+  write.table(da, paste("maf001rsq08minimums_chr",i,".txt",sep = ""), col.names = F, row.names = F, quote = F, sep = "\t")
 }
 
 q()
