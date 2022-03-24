2335246875e2d6f28de5e8322073464aa4871d32
Lynne Krohn <lynne.krohn@mail.mcgill.ca>
2022-03-24 09:48:20 -0400

    Update STEP3_download_after_impute.sh

--- STEP3_download_after_impute.sh
+++ STEP3_download_after_impute.sh
@@ -24,7 +24,7 @@ for chnum in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
   do
   plink --vcf chr${chnum}.dose.vcf.gz --make-bed --out s1 --double-id
   plink --bfile s1 --bmerge s1 --merge-mode 6 --out s8
-  plink --bfile s1 --exclude s8.missnp --make-bed --out s2
+  plink --bfile s1 --exclude s8.diff --make-bed --out s2
   plink --bfile s2 --list-duplicate-vars --out s4
   plink --bfile s2 --exclude s4.dupvar --make-bed --out s3
   plink --bfile s3 --qual-scores chr$chnum.info 7 1 1 --qual-threshold 0.3 --make-bed --out plink_files/soft_calls/chr${chnum}
@@ -35,7 +35,7 @@ for chnum in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
   do
   plink --vcf chr${chnum}.dose.vcf.gz --make-bed --out s1 --double-id
   plink --bfile s1 --bmerge s1 --merge-mode 6 --out s8
-  plink --bfile s1 --exclude s8.missnp --make-bed --out s2
+  plink --bfile s1 --exclude s8.diff --make-bed --out s2
   plink --bfile s2 --list-duplicate-vars --out s4
   plink --bfile s2 --exclude s4.dupvar --make-bed --out s3
   plink --bfile s3 --qual-scores chr$chnum.info 7 1 1 --qual-threshold 0.8 --make-bed --out plink_files/hard_calls/chr${chnum}
