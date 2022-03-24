d85de121503f92296549f12bd1b3b03602a414a2
Lynne Krohn <lynne.krohn@mail.mcgill.ca>
2022-03-24 09:50:45 -0400

    Update STEP7_GWAS_prepOutput.sh

--- STEP7_GWAS_prepOutput.sh
+++ STEP7_GWAS_prepOutput.sh
@@ -11,12 +11,16 @@ require(data.table)
 infos <- fread("allChrs.Info")
 colnames(infos) <- c("SNP","ALT_Frq","Rsq")
 assoc <- fread("allChrs.assoc")
-colnames(assoc) <- c("CHROM","POS","A2","A1","N","Test","Beta","SE","Pvalue")
+colnames(assoc) <- c("CHROM","POS","REF","ALT","N","Test","Beta","SE","Pvalue")
+
+#If SNP in infos is in the format CHR:POS:REF:ALT add this step
+assoc$Test <- paste(assoc$Test, assoc$REF, assoc$ALT, sep = ":")
 data <- merge(infos, assoc, by.x = "SNP", by.y = "Test", all.y = T)
 dat <- subset(data, Beta < 5 & Beta > -5 & !is.na(data$Pvalue))
-dat$chr <- paste("chr",dat$CHROM, sep = "")
+head(dat)
+
 #final columns below
-dat$SNP <- paste(dat$chr,dat$POS, sep = ":")
+dat$markerID <- paste(dat$CHROM,dat$POS, sep = ":")
 dat$minorAllele <- ifelse(dat$ALT_Frq <= 0.5, as.character(dat$ALT), as.character(dat$REF))
 dat$majorAllele <- ifelse(dat$ALT_Frq <= 0.5, as.character(dat$REF), as.character(dat$ALT))
 dat$beta <- ifelse(dat$ALT_Frq <= 0.5, dat$Beta, dat$Beta*-1)
@@ -26,8 +30,8 @@ dat$P <- dat$Pvalue
 dat$CHR <- dat$CHROM
 dat$BP <- dat$POS
 dat$OR <- exp(dat$beta)
-dat0 <- dat[,c("CHR","BP","SNP","minorAllele","majorAllele","beta", "OR","se","maf","P")]
-write.table(dat0, "GWAS.YOUR_NAME.tab", quote = F, sep = "\t", row.names = F)
+dat0 <- dat[,c("markerID","minorAllele","majorAllele","beta","OR","se","maf","P","N")]
+write.table(dat0, "toMeta.YOUR_NAME.tab", quote = F, sep = "\t", row.names = F)
 q()
 n
  
